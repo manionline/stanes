1c418ec8e87aee8ca6b00203d007640cd5e5b342
manionline <manionline@yahoo.com>
2014-03-04 12:18:36 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stanes::Application.config.secret_key_base = 'ace80a75b49cf4e1fcedd19d397d5c6aec8ce676e37c65a087e816cee53f46bf86ab46bb9dc0e19cd7de5b2cd832b201a88b30b8f3de0d860a58b299a34e9bee'
